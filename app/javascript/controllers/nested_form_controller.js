5a82913ddbb99d36fba76937be9381adb6504416
ShueJadie <joshue.digiteer@gmail.com>
2023-10-01 20:59:36 +0800

    Updated nested form code for products

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="nested-form"
 export default class extends Controller {
   // Used for setting data-target (refer to the ProductVariant nested form in products/index.html.slim)
-  static targets = ["add_product_variant", "template"]
+  static targets = ["add_item", "template"]
 
   add_association(e) {
     // Prevents the event's default action from happening
@@ -13,10 +13,10 @@ export default class extends Controller {
     // templateTarget are objects created by StimulusJS through "static targets"
 
     // TEMPLATE_RECORD is the value of child_index in the template part of the ProductVariant nested form (refer to products/index.html.slim)
-    let content = this.templateTarget.innerHTML.replace(/PRODUCT_VARIANT_RECORD/g, new Date().valueOf())
+    let content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
 
     // beforebegin adds the HTML code above the caller (Add Product Variant link_to)
-    this.add_product_variantTarget.insertAdjacentHTML('beforebegin', content)
+    this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
   }
 
   remove_association(e) {

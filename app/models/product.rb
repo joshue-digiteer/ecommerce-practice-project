9081bacf45bb970faa7da49484c7a6934858c72f
ShueJadie <joshue.digiteer@gmail.com>
2023-10-03 12:05:49 +0800

    Converted filter process in index action of product to a scope in the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,13 +20,6 @@ class Product < ApplicationRecord
 	# accepts_nested_attributes_for :product_variants, reject_if: ->(attr){ attr['name'].blank? }, allow_destroy: true
 
 	# Filter
-	SORT_OPTIONS = {
-		"name_asc" => {name: :asc},
-		"name_desc" => {name: :desc},
-		"price_asc" => {price: :asc},
-		"price_desc" => {price: :desc}
-	}
-
 	scope :filter_products, -> (filter_params) {
 		products = all
 
@@ -37,6 +30,12 @@ class Product < ApplicationRecord
 		products = products.where("quantity > 0") if filter_params[:stock].present?
 
 		# Sort
-		products = products.order(SORT_OPTIONS[filter_params[:sort]]) if filter_params[:sort].present?
+		sort_options = {
+			"name_asc" => {name: :asc},
+			"name_desc" => {name: :desc},
+			"price_asc" => {price: :asc},
+			"price_desc" => {price: :desc}
+		}
+		products = products.order(sort_options[filter_params[:sort]]) if filter_params[:sort].present?
 	}
 end
